c9c3b74e06a5e448078a71ae557121b75c7c999a
Gee1111 <40726821+Gee1111@users.noreply.github.com>
2022-11-21 23:36:36 +0100

    Update wizard.c

--- src/wizard.c
+++ src/wizard.c
@@ -685,9 +685,6 @@ wizard_page_t *wizard_network(const char *lang)
 
   for (idx = 0; idx < ARRAY_SIZE(props); idx++)
     w->props[idx] = props[idx];
-
-    if (!tvh_inputs.lh_first)
-    return page;
   
   for (ti = LIST_LAST(tvh_input_t, &tvh_inputs, ti_link); ti;
        ti = LIST_PREV(ti, tvh_input_t, &tvh_inputs, ti_link)) {
